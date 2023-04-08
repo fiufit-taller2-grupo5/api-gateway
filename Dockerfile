1825bfcc118abf8d700e83f123058a7cb37eadaf
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-04-08 16:08:05 -0300

    Creates api gateway

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# We start from a node 18 image using alpine
+FROM node:18-alpine
+
+# The workdir inside the container where the following commands will be executed
+WORKDIR /app
+
+# Copy the package.json and package-lock.json files into the workdir
+COPY package*.json ./
+
+# Installs dependencies
+RUN npm install
+
+# Copies the rest of the files into the workdir 
+COPY . .
+
+RUN npx prisma generate
+
+# Builds the app
+RUN npm run build
+
+# Exposes the port 8181
+EXPOSE 8181
+
+# Runs the app
+CMD [ "npm", "start" ]
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,58 @@
 import express, { Express, Request, Response } from 'express';
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 const app: Express = express();
 
-const port = 3030;
-app.get("/", (req: Request, res: Response) => {
+const port = 8181;
+
+app.get("/", (_req: Request, res: Response) => {
     console.log("Hello");
     res.send("Hello");
 });
 
+app.get("/health", (_req, res) => {
+    res.send("Healthcheck OK");
+});
+
+const getAxiosConfigFromRequest = (req: Request, serviceUrl: string): AxiosRequestConfig => {
+    return {
+        method: req.method,
+        url: serviceUrl,
+        headers: req.headers,
+        data: req.body,
+    };
+}
+
 app.get('/user-service/**', async (req: Request, res: Response) => {
-    const userServicePort = 30000;
+    const userServicePort = 3000;
     const userServiceUrl = `http://user-service:${userServicePort}${req.url.replace('/user-service', '')}`;
-
     try {
-        const config = {
-            method: req.method,
-            url: userServiceUrl,
-            headers: req.headers,
-            data: req.body,
-        };
-
-        console.log(config);
-
-        const response = await axios(config);
+        const response = await axios(getAxiosConfigFromRequest(req, userServiceUrl));
         res.send(response.data);
     } catch(err: any) {
-        res.status(err.response.status).send(err.response.data);
+        if (err && err.response) {
+            res.status(err.response.status).send(err.response.data);
+        } else {
+            res.status(500).send(err.message);
+        }   
     }
-    
+});
+
+app.get("/training-service/**", async (req: Request, res: Response) => {
+    const traningServicePort = 3000;
+    const traningServiceUrl = `http://training-service:${traningServicePort}${req.url.replace('/training-service', '')}`;
+    try {
+        const response = await axios(getAxiosConfigFromRequest(req, traningServiceUrl));
+        res.send(response.data);
+    } catch(err: any) {
+        if (err && err.response) {
+            res.status(err.response.status).send(err.response.data);
+        } else {
+            res.status(500).send(err.message);
+        }
+    }
 });
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server :) is running at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server :) is running at port ${port}`);
 });
