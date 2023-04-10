ed64f7755b14578beea5c5c95277dd0ed062ba7c
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-04-09 21:56:53 -0300

    Sends 401 when not authorized and add logs

--- src/auth-middleware.ts
+++ src/auth-middleware.ts
@@ -18,11 +18,13 @@ interface AuthenticatedRequest extends Request {
 export const decodeFirebaseIdToken = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
   const headers = req.headers;
   if (headers.dev) {
+    console.log("User in dev mode");
     return next();
   }
 
   if (!req.headers.id_token) {
-    return res.status(400).json({
+    console.log("User not authenticated");
+    return res.status(401).json({
       error: {
         message: 'You did not specify any idToken for this request',
       },
@@ -32,8 +34,10 @@ export const decodeFirebaseIdToken = async (req: AuthenticatedRequest, res: Resp
   try {
     const userPayload: UserPayload = await firebase.auth().verifyIdToken(req.headers.id_token as string);
     req.user = userPayload;
+    console.log("User validated");
     next();
   } catch (error) {
+    console.log("Error while decoding id token", error);
     return res.status(500).json({
       error,
     });
