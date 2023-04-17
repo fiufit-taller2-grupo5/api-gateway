7cb53586fdb96c0f39b180d6ec2a4f51f0fc60cd
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-04-17 17:46:54 -0300

    removes unused logs

--- src/middleware/swaggerDocs.ts
+++ src/middleware/swaggerDocs.ts
@@ -1,14 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const swaggerDocs = (req: Request, _res: Response, next: NextFunction) => {
-    console.log("Received request with path: " + req.path + " and method: " + req.method);
     if (req.originalUrl.includes("user-service/swagger-ui") && req.method === "GET") {
-        console.log("Modifying path to suite swagger-ui");
-        console.log("New path: " + req.originalUrl.replace("user-service/swagger-ui", "user-service/docs/swagger-ui"));
         req.url = req.originalUrl.replace("user-service/swagger-ui", "user-service/docs/swagger-ui");
-        console.log("Now url is " + req.url);
-    } else {
-        console.log("Not a swagger request");
     }
 
     next();
