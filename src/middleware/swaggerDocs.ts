e78b64c49686a1e2e009a0d9a8cb9452edb84a9f
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-04-17 17:33:41 -0300

    Fix bugs

--- src/middleware/swaggerDocs.ts
+++ src/middleware/swaggerDocs.ts
@@ -4,7 +4,7 @@ export const swaggerDocs = (req: Request, _res: Response, next: NextFunction) =>
     console.log("Received request with path: " + req.path + " and method: " + req.method);
     if (req.path.includes("user-service/swagger-ui") && req.method === "GET") {
         console.log("Modifying path to suite swagger-ui");
-        req.path.replace("user-service/", "user-service/docs/");
+        req.path = req.path.replace("user-service/", "user-service/docs/");
         console.log("Not path is " + req.path);
     } else {
         console.log("Not a swagger request");
