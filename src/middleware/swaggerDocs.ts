e425f1e062ec72003ae9ee829e39882caaa4f5a7
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-04-17 17:41:23 -0300

    Fixes bug

--- src/middleware/swaggerDocs.ts
+++ src/middleware/swaggerDocs.ts
@@ -2,10 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 
 export const swaggerDocs = (req: Request, _res: Response, next: NextFunction) => {
     console.log("Received request with path: " + req.path + " and method: " + req.method);
-    if (req.path.includes("user-service/swagger-ui") && req.method === "GET") {
+    if (req.originalUrl.includes("user-service/swagger-ui") && req.method === "GET") {
         console.log("Modifying path to suite swagger-ui");
-        req.path = req.path.replace("user-service/", "user-service/docs/");
-        console.log("Not path is " + req.path);
+        console.log("New path: " + req.originalUrl.replace("user-service/swagger-ui", "user-service/docs/swagger-ui"));
+        req.url = req.originalUrl.replace("user-service/swagger-ui", "user-service/docs/swagger-ui");
+        console.log("Now url is " + req.url);
     } else {
         console.log("Not a swagger request");
     }
