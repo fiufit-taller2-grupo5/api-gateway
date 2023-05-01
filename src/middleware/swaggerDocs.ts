63f13c4e4de956b1b6190f6105de8b88441ae982
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-05-01 15:26:26 -0300

    Refactors on redirections

--- src/middleware/swaggerDocs.ts
+++ src/middleware/swaggerDocs.ts
@@ -1,17 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
 
-export const swaggerDocs = (req: Request, _res: Response, next: NextFunction) => {
+const redirectToUserServiceDocsIfNecessary = (req: Request) => {
     if (req.originalUrl.includes("user-service/swagger-ui") && req.method === "GET") {
         req.url = req.originalUrl.replace("user-service/swagger-ui", "user-service/docs/swagger-ui");
-    } else if (req.originalUrl.includes("openapi.json") && req.method == "GET") {
-        console.log("Req includes /openapi.json");
+    }
+}
+
+const redirectToTrainingServiceDocsIsNecessary = (req: Request) => {
+    if (req.originalUrl.includes("openapi.json") && req.method == "GET") {
         if(req.headers["referer"] && req.headers["referer"]?.includes("training-service/docs")) {
-            console.log("Redirecting correctly to training-service docs");
             req.url = req.originalUrl.replace("openapi.json", "training-service/openapi.json");
-        } else {
-            console.log("Not redirecting");
         }
     }
+}
 
+export const swaggerDocs = (req: Request, _res: Response, next: NextFunction) => {
+    redirectToUserServiceDocsIfNecessary(req);
+    redirectToTrainingServiceDocsIsNecessary(req);
     next();
 }
