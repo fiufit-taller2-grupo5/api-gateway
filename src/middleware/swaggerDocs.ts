6aba9c4c0acaf717f57dfe2740d5233808568b85
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-05-01 15:20:14 -0300

    Adds support for docs in training-service with logs

--- src/middleware/swaggerDocs.ts
+++ src/middleware/swaggerDocs.ts
@@ -3,6 +3,14 @@ import { Request, Response, NextFunction } from 'express';
 export const swaggerDocs = (req: Request, _res: Response, next: NextFunction) => {
     if (req.originalUrl.includes("user-service/swagger-ui") && req.method === "GET") {
         req.url = req.originalUrl.replace("user-service/swagger-ui", "user-service/docs/swagger-ui");
+    } else if (req.originalUrl.includes("openapi.json") && req.method == "GET") {
+        console.log("Req includes /openapi.json");
+        if(req.headers["referer"] && req.headers["referer"]?.includes("training-service/docs")) {
+            console.log("Redirecting correctly to training-service docs");
+            req.url = req.originalUrl.replace("openapi.json", "training-service/openapi.json");
+        } else {
+            console.log("Not redirecting");
+        }
     }
 
     next();
